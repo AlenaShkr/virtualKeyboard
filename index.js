1c4f9c916ad0f6a1ef562514d09d210181650d0b
AlenaShkr <elalshkr@gmail.com>
2020-02-05 23:01:48 +0300

    feat: add rus massive label button

--- index.js
+++ index.js
@@ -5,21 +5,32 @@ window.onload = function load() {
 
   const keyboard = document.createElement('div');
   keyboard.className = 'keyboard';
-  const valueKey = [
+  const valueKeyEng = [
     ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'bsp'],
-    ['tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'ent'],
-    ['caps', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", '/', 'ent'],
-    ['shift', '|', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'shift', ''],
-    ['tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'ent'],
-    ['tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'ent']];
+    ['tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '|'],
+    ['caps', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'ent', ''],
+    ['shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'shift', 'up'],
+    ['ctrl', 'fn', 'win', 'alt', 'gap', 'alt', 'list', 'ctl', '', '', '', '<-', 'down', '->']];
 
-  for (let i = 0; i < 6; i += 1) {
+  const valueKeyRus = [
+    ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'bsp'],
+    ['tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '|'],
+    ['caps', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'ent', ''],
+    ['shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'shift', 'up'],
+    ['ctrl', 'fn', 'win', 'alt', 'gap', 'alt', 'list', 'ctl', '', '', '', '<-', 'down', '->']];
+
+  const objectLang = {
+    eng: valueKeyEng,
+    rus: valueKeyRus,
+  };
+
+  for (let i = 0; i < 5; i += 1) {
     for (let j = 0; j < 14; j += 1) {
       const btn = document.createElement('button');
       btn.className = 'button';
 
-      btn.innerHTML = valueKey[i][j];
-      btn.value = valueKey[i][j];
+      btn.innerHTML = objectLang.rus[i][j];
+      btn.value = objectLang.rus[i][j];
       keyboard.appendChild(btn);
     }
   }
