079d5697fa67d3f3198fa663b208d66de7365d8c
”AlenaShkr” <elalshkr@gmail.com>
2020-12-21 23:14:13 +0300

    change code style

--- js/index.js
+++ js/index.js
@@ -1,17 +1,7 @@
 import objectLang from './layout';
+import drawStartPage from './startPage';
 
-function drawButtons(keyboard, lang) {
-  for (let i = 0; i < 5; i += 1) {
-    for (let j = 0; j < 14; j += 1) {
-      const btn = document.createElement('button');
-      btn.className = 'button';
 
-      btn.innerHTML = objectLang[lang][i][j];
-      btn.value = objectLang[lang][i][j];
-      keyboard.appendChild(btn);
-    }
-  }
-}
 function redrawButtons(keyboard, lang) {
   const buttons = keyboard.querySelectorAll('.button');
   const result = [].concat(...objectLang[lang]);
@@ -20,30 +10,10 @@ function redrawButtons(keyboard, lang) {
     buttons[i].value = result[i];
   }
 }
-window.onload = function load() {
-  const textField = document.createElement('textarea');
-  textField.className = 'text';
-  document.body.appendChild(textField);
-  const keyboard = document.createElement('div');
-  keyboard.className = 'keyboard';
-  const keyboardButton = document.createElement('div');
-  keyboardButton.className = 'keyboard-buttons';
-  const sectionChoiceLang = document.createElement('section');
-  sectionChoiceLang.className = 'slider-checkbox';
-  const checkboxChoiceLang = document.createElement('input');
-  checkboxChoiceLang.type = 'checkbox';
-  checkboxChoiceLang.className = 'checkbox';
-  const labelForCheckbox = document.createElement('label');
-  labelForCheckbox.className = 'label';
-  labelForCheckbox.htmlFor = 'checkbox';
-  sectionChoiceLang.appendChild(checkboxChoiceLang);
-  sectionChoiceLang.appendChild(labelForCheckbox);
-  keyboard.appendChild(sectionChoiceLang);
-  let lang = 'rus';
 
-  drawButtons(keyboardButton, lang);
-  keyboard.appendChild(keyboardButton);
-  document.body.appendChild(keyboard);
+window.onload = function load() {
+  const { textField, checkboxChoiceLang, keyboardButton } = drawStartPage();
+  let lang = 'rus';
   checkboxChoiceLang.addEventListener('click', () => {
     lang = 'rus';
     if (checkboxChoiceLang.checked) {
--- js/startPage.js
+++ js/startPage.js
@@ -1,19 +1,43 @@
-// const textField = document.createElement('textarea');
-//   textField.className = 'text';
-//   document.body.appendChild(textField);
-//   const keyboard = document.createElement('div');
-//   keyboard.className = 'keyboard';
-//   const keyboardButton = document.createElement('div');
-//   keyboardButton.className = 'keyboard-buttons';
-//   const sectionChoiceLang = document.createElement('section');
-//   sectionChoiceLang.className = 'slider-checkbox';
-//   const checkboxChoiceLang = document.createElement('input');
-//   checkboxChoiceLang.type = 'checkbox';
-//   checkboxChoiceLang.className = 'checkbox';
-//   const labelForCheckbox = document.createElement('label');
-//   labelForCheckbox.className = 'label';
-//   labelForCheckbox.htmlFor = 'checkbox';
-//   sectionChoiceLang.appendChild(checkboxChoiceLang);
-//   sectionChoiceLang.appendChild(labelForCheckbox);
-//   keyboard.appendChild(sectionChoiceLang);
-//   let lang = 'rus';
+import objectLang from './layout';
+
+function drawButtons(keyboard, lang) {
+  for (let i = 0; i < 5; i += 1) {
+    for (let j = 0; j < 14; j += 1) {
+      const btn = document.createElement('button');
+      btn.className = 'button';
+
+      btn.innerHTML = objectLang[lang][i][j];
+      btn.value = objectLang[lang][i][j];
+      keyboard.appendChild(btn);
+    }
+  }
+}
+
+function drawStartPage() {
+  const textField = document.createElement('textarea');
+  textField.className = 'text';
+  document.body.appendChild(textField);
+  const keyboard = document.createElement('div');
+  keyboard.className = 'keyboard';
+  const keyboardButton = document.createElement('div');
+  keyboardButton.className = 'keyboard-buttons';
+  const sectionChoiceLang = document.createElement('section');
+  sectionChoiceLang.className = 'slider-checkbox';
+  const checkboxChoiceLang = document.createElement('input');
+  checkboxChoiceLang.type = 'checkbox';
+  checkboxChoiceLang.className = 'checkbox';
+  const labelForCheckbox = document.createElement('label');
+  labelForCheckbox.className = 'label';
+  labelForCheckbox.htmlFor = 'checkbox';
+  sectionChoiceLang.appendChild(checkboxChoiceLang);
+  sectionChoiceLang.appendChild(labelForCheckbox);
+  keyboard.appendChild(sectionChoiceLang);
+  const lang = 'rus';
+
+  drawButtons(keyboardButton, lang);
+  keyboard.appendChild(keyboardButton);
+  document.body.appendChild(keyboard);
+  return { textField, checkboxChoiceLang, keyboardButton };
+}
+
+export default drawStartPage;
