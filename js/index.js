01cdaca26c906c2b25a5403cce0ba4e00623668e
”AlenaShkr” <elalshkr@gmail.com>
2020-12-21 22:41:34 +0300

    change code style

--- js/index.js
+++ js/index.js
@@ -1,21 +1,5 @@
-const valueKeyEng = [
-  ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'bsp'],
-  ['tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '|'],
-  ['caps', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'ent', ''],
-  ['shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'shift', 'up'],
-  ['ctrl', 'fn', 'win', 'alt', 'gap', 'alt', 'list', 'ctl', '', '', '', '<-', 'down', '->']];
+import objectLang from './layout';
 
-const valueKeyRus = [
-  ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'bsp'],
-  ['tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '|'],
-  ['caps', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'ent', ''],
-  ['shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'shift', 'up'],
-  ['ctrl', 'fn', 'win', 'alt', 'gap', 'alt', 'list', 'ctl', '', '', '', '<-', 'down', '->']];
-
-const objectLang = {
-  eng: valueKeyEng,
-  rus: valueKeyRus,
-};
 function drawButtons(keyboard, lang) {
   for (let i = 0; i < 5; i += 1) {
     for (let j = 0; j < 14; j += 1) {
@@ -74,7 +58,7 @@ window.onload = function load() {
     if (event.key === 'Tab') {
       textField.value += '\t';
     }
-    if (event.key !== 'Control' || 'Shift' || 'Tab') {
+    if (event.key !== 'Control' && 'Shift' && 'Tab') {
       textField.value += event.key;
     }
   });
